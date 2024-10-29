948ed5e13cb41a083abe806b8e96046f310c9e6b
jakub <kuba.furtak.pl@gmail.com>
2024-10-29 10:23:46 +0100

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by student on 29/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
